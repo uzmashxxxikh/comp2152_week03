7303fc83b2f28671a970a76ef597f90f3e87a686
uzmashxxxikh <uzmashxikh@hotmail.com>
2025-01-21 11:27:40 -0500

    fuxed the Inputs

--- lab03.py
+++ lab03.py
@@ -8,16 +8,22 @@ weapons = ['Fist', 'Knife', 'Club', 'Gun', 'Bomb', 'Nuclear Bomb']
 
 print("Available Weapons:", ', '.join(weapons))
 
-# Inputs
+# # Inputs
+combatStrength = input("Enter your combat strength (1-6): ")
+if combatStrength < 1 or combatStrength > 6:
+    print("Invalid input. Please enter a number between 1 and 6.")
+    combatStrength = 1
+    exit()
+
 combatStrength = max(1, min(6, int(input("Hero strength (1-6): "))))
 mCombatStrength = max(1, min(6, int(input("Monster strength (1-6): "))))
 
 # Battle
-for j in range(1, 21, 2):
-    heroRoll, monsterRoll = random.choice(diceOptions), random.choice(diceOptions)
-    heroTotal, monsterTotal = combatStrength + heroRoll, mCombatStrength + monsterRoll
-    print(f"Round {j}: Hero({weapons[heroRoll - 1]})={heroTotal}, Monster({weapons[monsterRoll - 1]})={monsterTotal}.", 
-          "Hero wins!" if heroTotal > monsterTotal else "Monster wins!" if heroTotal < monsterTotal else "Tie!")
-    if j == 11:
-        print("Battle Truce declared. Game Over!")
-        break
+# for j in range(1, 21, 2):
+#     heroRoll, monsterRoll = random.choice(diceOptions), random.choice(diceOptions)
+#     heroTotal, monsterTotal = combatStrength + heroRoll, mCombatStrength + monsterRoll
+#     print(f"Round {j}: Hero({weapons[heroRoll - 1]})={heroTotal}, Monster({weapons[monsterRoll - 1]})={monsterTotal}.", 
+#           "Hero wins!" if heroTotal > monsterTotal else "Monster wins!" if heroTotal < monsterTotal else "Tie!")
+#     if j == 11:
+#         print("Battle Truce declared. Game Over!")
+#         break
